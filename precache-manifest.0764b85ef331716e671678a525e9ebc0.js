7cf994c6ad7707790a5c6b04bff0f81b2bb7a159
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-08-19 20:46:10 -0700

    Updates

---
+++ precache-manifest.0764b85ef331716e671678a525e9ebc0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c73e2c4b5e53da55d9720771f9c289f",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "ae179b32e7b663e94150",
+    "url": "/Clicky-Game/static/css/main.5f4b3aa4.chunk.css"
+  },
+  {
+    "revision": "9acba71f41afb7620073",
+    "url": "/Clicky-Game/static/js/2.937769ed.chunk.js"
+  },
+  {
+    "revision": "0c642dbff179889abda1a42753a27d19",
+    "url": "/Clicky-Game/static/js/2.937769ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae179b32e7b663e94150",
+    "url": "/Clicky-Game/static/js/main.131024a3.chunk.js"
+  },
+  {
+    "revision": "f70336978153cff784ba",
+    "url": "/Clicky-Game/static/js/runtime-main.a0ce3ec2.js"
+  },
+  {
+    "revision": "1765eddf8e86d150b37c7fb1417bd2e8",
+    "url": "/Clicky-Game/static/media/You Lose - Sound Effect - 2017.1765eddf.mp3"
+  },
+  {
+    "revision": "c7551b0560cf795709d3696c2d68e41f",
+    "url": "/Clicky-Game/static/media/You Win (Voice) - Sound Effect [HD].c7551b05.mp3"
+  },
+  {
+    "revision": "ecf83eac8834d9bd1c1c5d110e14b80b",
+    "url": "/Clicky-Game/static/media/correct sound effect.ecf83eac.mp3"
+  },
+  {
+    "revision": "7e7c97af850fe70c8d1cd2eefaac5429",
+    "url": "/Clicky-Game/static/media/maxresdefault.7e7c97af.jpg"
+  }
+]);
